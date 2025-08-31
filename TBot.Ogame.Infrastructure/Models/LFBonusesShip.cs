680e7622e6b7f55642b86872b65773a96c16db71
Cheeeh <oinco.h@gmail.com>
2025-08-31 20:40:17 +0200

    update LFBonuses

--- TBot.Ogame.Infrastructure/Models/LFBonusesShip.cs
+++ TBot.Ogame.Infrastructure/Models/LFBonusesShip.cs
@@ -6,22 +6,22 @@ using System.Threading.Tasks;
 
 namespace TBot.Ogame.Infrastructure.Models {
 	public class LFBonusesShip {
-		public float Armour { get; set; }
-		public float Shield { get; set; }
-        public float Weapon { get; set; }
-        public float Cargo { get; set; }
-        public float Speed { get; set; }
-        public float Consumption { get; set; }
-        public float Duration { get; set; }
+		public int ID { get; set; }
+		public float StructuralIntegrity { get; set; }
+		public float ShieldPower { get; set; }
+		public float WeaponPower { get; set; }
+		public float Speed { get; set; }
+		public float CargoCapacity { get; set; }
+		public float FuelConsumption { get; set; }
 
-		public LFBonusesShip(float armour = 0, float shield = 0, float weapon = 0, float cargo = 0, float speed = 0, float consumption = 0, float duration = 0) {
-			Armour = armour;
-			Shield = shield;
-			Weapon = weapon;
-			Cargo = cargo;
+		public LFBonusesShip(int id = -1, float armour = 0, float shield = 0, float weapon = 0, float speed = 0, float cargo = 0, float consumption = 0) {
+			ID = id;
+			StructuralIntegrity = armour;
+			ShieldPower = shield;
+			WeaponPower = weapon;
 			Speed = speed;
-			Consumption = consumption;
-			Duration = duration;
+			CargoCapacity = cargo;
+			FuelConsumption = consumption;
 		}
     }
 }
